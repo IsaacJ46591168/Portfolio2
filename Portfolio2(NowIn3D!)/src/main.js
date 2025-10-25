0d06bc0eae2facda481d0625cb557be9d5fdf003
IsaacJ46591168 <isaac.james@students.mq.edu.au>
2025-10-25 14:40:21 +0100

    Spinning torus
    Added a torus to the webpage renderer and made it spin around

--- Portfolio2(NowIn3D!)/src/main.js
+++ Portfolio2(NowIn3D!)/src/main.js
@@ -3,9 +3,9 @@ import * as THREE from 'three'
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth, window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGL3DRenderer({
+const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#background"),
 });
 
@@ -17,13 +17,15 @@ const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true})
 const torus = new THREE.Mesh(geometry, material);
 
+scene.add(torus)
+
 function animate()
 {
-  requestAnimationFrame(animate)
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.05;
+  torus.rotation.z += 0.01;
   renderer.render(scene, camera);
-  torus.rotation.x += 0.1;
-  torus.rotation.y += 0.1;
-  torus.rotation.z += 0.1;
 }
 
 animate();
