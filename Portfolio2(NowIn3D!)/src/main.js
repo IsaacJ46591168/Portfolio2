00b8b6b5ec67d4eee71a9befa41b24419257f5f8
IsaacJ46591168 <isaac.james@students.mq.edu.au>
2025-10-27 19:08:07 +0000

    Rectangle lighting
    Added some rectangle lights to act as the screens for the monitor and the laptop screen

--- Portfolio2(NowIn3D!)/src/main.js
+++ Portfolio2(NowIn3D!)/src/main.js
@@ -1,4 +1,4 @@
-import { GLTFLoader, OrbitControls } from 'three/examples/jsm/Addons.js';
+import { GLTFLoader, OrbitControls, RectAreaLightHelper } from 'three/examples/jsm/Addons.js';
 import './style.css'
 import * as THREE from 'three'
 
@@ -23,17 +23,17 @@ document.body.appendChild(renderer.domElement)
 const camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0.2, 6, 15);
 // camera.lookAt(0, 0, 0);
-// const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-// controls.enableDamping = false;
-// controls.enablePan = false;
-// controls.minDistance = 5;
-// controls.maxDistance = 20;
-// controls.minPolarAngle = 0.5;
-// controls.maxPolarAngle = 1.5;
-// controls.autoRotate = false;
-// controls.target = new THREE.Vector3(0, 6, 2)
-// controls.update();
+controls.enableDamping = false;
+controls.enablePan = false;
+controls.minDistance = 5;
+controls.maxDistance = 50;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 6, 2)
+controls.update();
 
 
 const loader = new GLTFLoader().setPath('DeskModel/')
@@ -63,17 +63,57 @@ const groundMaterial = new THREE.MeshStandardMaterial({color: 0xffffff, side: TH
 const groundMesh = new THREE.Mesh(groundPlane, groundMaterial);
 scene.add(groundMesh);
 
-//Basic Light
-const spotlight = new THREE.SpotLight(0xffffff, 2000, 100, 1, 0.5);
-spotlight.position.set(0, 25, 0);
+//Basic spotlight
+const spotlight = new THREE.SpotLight(0xffffff, 100, 10, 1.1, 0.5);
+spotlight.position.set(0, 10, 2);
 spotlight.castShadow = true;
-spotlight.shadow.bias = -0.0001;
+spotlight.shadow.mapSize.width = 2048;
+spotlight.shadow.mapSize.height = 2048;
+spotlight.shadow.bias = -0.0005;
 scene.add(spotlight);
+
+// let spotHelper = new THREE.SpotLightHelper(spotlight, 0xffffff);
+// spotlight.add(spotHelper);
+
+// //Ambient light
+// const ambLight = new THREE.AmbientLight(0xffffff, 1);
+// scene.add(ambLight);
+
+//Monitor Backlight
+const monitorLight = new THREE.RectAreaLight(0xffdb87, 10, 3.6, 1.8);
+monitorLight.position.set(0.04, 5.4, -2.5);
+monitorLight.rotateX(Math.PI)
+scene.add(monitorLight);
+
+let monHelper = new RectAreaLightHelper(monitorLight, 0xffdb87);
+monitorLight.add(monHelper);
+
+//Laptop backlight
+const laptopLight = new THREE.RectAreaLight(0x2128b5, 10, 1.6, 0.83);
+laptopLight.position.set(-2.66, 4.52, -2.01);
+laptopLight.rotateX(Math.PI)
+laptopLight.rotateY(-36.2 * (Math.PI / 180))
+scene.add(laptopLight);
+
+let lapHelper = new RectAreaLightHelper(laptopLight, 0xffffff);
+laptopLight.add(lapHelper);
+
+
+
+// //Directional light
+// const dirLight = new THREE.DirectionalLight(0xffffff, 5.0);
+// dirLight.position.set(0, 4, 8);
+// dirLight.target.position.set(0, 3, 0);
+// dirLight.castShadow = true;
+// scene.add(dirLight);
+
+// let dirLighthelper = new THREE.DirectionalLightHelper(dirLight, 5);
+// dirLight.add(dirLighthelper);
 
 function animate()
 {
   requestAnimationFrame(animate);
-  // controls.update();
+  controls.update();
   renderer.render(scene, camera);
 }
 
