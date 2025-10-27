3679c0016859a437b91bad98da0e6f544bfe23b2
IsaacJ46591168 <isaac.james@students.mq.edu.au>
2025-10-27 23:04:18 +0000

    Tri-light setup
    Placed some more lights into the scene to try and create the mood that I'm going for. Put 2 spotlights at the POV of the camera and to the right of the camera, and put a directional light to the right of the camera to act as the sun coming through a window. Had to disable the shadows on the directional light as the shadows it was generating were not pleasant

--- Portfolio2(NowIn3D!)/src/main.js
+++ Portfolio2(NowIn3D!)/src/main.js
@@ -21,8 +21,9 @@ document.body.appendChild(renderer.domElement)
 
 //Camera setup
 const camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0.2, 6, 15);
-// camera.lookAt(0, 0, 0);
+camera.position.set(0.2, 6, 16);
+// camera.rotateY(20 * (Math.PI / 180))
+// camera.lookAt(5, 5, 5);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 controls.enableDamping = false;
@@ -46,8 +47,6 @@ loader.load('portfolio2.gltf', (gltf) => { //Callback function (active when othe
     child.castShadow = true;
     child.receiveShadow = true;
   })
-  
-
   scene.add(mesh);
 });
 
@@ -64,20 +63,66 @@ const groundMesh = new THREE.Mesh(groundPlane, groundMaterial);
 scene.add(groundMesh);
 
 //Basic spotlight
-const spotlight = new THREE.SpotLight(0xffffff, 100, 10, 1.1, 0.5);
-spotlight.position.set(0, 10, 2);
-spotlight.castShadow = true;
-spotlight.shadow.mapSize.width = 2048;
-spotlight.shadow.mapSize.height = 2048;
-spotlight.shadow.bias = -0.0005;
-scene.add(spotlight);
+// const spotlight = new THREE.SpotLight(0xf0aa4f, 10, 100, 1.1, 0.5);
+// spotlight.position.set(0, 10, 0);
+// spotlight.rotateZ(-Math.PI / 2);
+// spotlight.castShadow = true;
+// spotlight.shadow.mapSize.width = 2048;
+// spotlight.shadow.mapSize.height = 2048;
+// spotlight.shadow.bias = -0.0005;
+// scene.add(spotlight);
 
-// let spotHelper = new THREE.SpotLightHelper(spotlight, 0xffffff);
-// spotlight.add(spotHelper);
+// let topLight = new THREE.SpotLightHelper(spotlight, 0xffffff);
+// spotlight.add(topLight);
 
-// //Ambient light
-// const ambLight = new THREE.AmbientLight(0xffffff, 1);
-// scene.add(ambLight);
+
+
+//Ambient light
+const ambLight = new THREE.AmbientLight(0x23227A, 10);
+scene.add(ambLight);
+
+//Window light
+const dirLight = new THREE.DirectionalLight(0xf0aa4f, 1);
+dirLight.position.set(2, 4, 4);
+dirLight.target.position.set(0, 3, 0);
+// dirLight.castShadow = true;
+// dirLight.shadow.camera.left = -5;
+// dirLight.shadow.camera.right = 1;
+// dirLight.shadow.camera.top = 5;
+// dirLight.shadow.camera.bottom = -5;
+// dirLight.shadow.bias = -0.0001;
+scene.add(dirLight);
+
+// let dirLighthelper = new THREE.DirectionalLightHelper(dirLight, 5);
+// dirLight.add(dirLighthelper);
+
+//Side Light
+const sideLight = new THREE.SpotLight(0x350dff, 1000, 70, 2, 0.5);
+sideLight.position.set(8, 10, 5);
+sideLight.target.position.set(0, 0, 0);
+sideLight.castShadow = true;
+sideLight.shadow.mapSize.width = 2048;
+sideLight.shadow.mapSize.height = 2048;
+sideLight.shadow.bias = -0.0001;
+scene.add(sideLight);
+
+// let sLightHelper = new THREE.SpotLightHelper(sideLight, 0xffffff);
+// sideLight.add(sLightHelper);
+
+//Front light
+const frontLight = new THREE.SpotLight(0x350dff, 200, 100, 2, 0.5);
+frontLight.position.set(0, 6, 6);
+frontLight.target.position.set(0, 0, 0);
+frontLight.castShadow = true;
+frontLight.shadow.mapSize.width = 2048;
+frontLight.shadow.mapSize.height = 2048;
+frontLight.shadow.bias = -0.0009;
+scene.add(frontLight);
+
+let fLightHelper = new THREE.SpotLightHelper(frontLight, 0xffffff);
+frontLight.add(fLightHelper);
+
+
 
 //Monitor Backlight
 const monitorLight = new THREE.RectAreaLight(0xffdb87, 10, 3.6, 1.8);
@@ -98,17 +143,6 @@ scene.add(laptopLight);
 let lapHelper = new RectAreaLightHelper(laptopLight, 0xffffff);
 laptopLight.add(lapHelper);
 
-
-
-// //Directional light
-// const dirLight = new THREE.DirectionalLight(0xffffff, 5.0);
-// dirLight.position.set(0, 4, 8);
-// dirLight.target.position.set(0, 3, 0);
-// dirLight.castShadow = true;
-// scene.add(dirLight);
-
-// let dirLighthelper = new THREE.DirectionalLightHelper(dirLight, 5);
-// dirLight.add(dirLighthelper);
 
 function animate()
 {
